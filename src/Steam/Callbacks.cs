6c36cef652f1c977b5bfbc104cdd6fc8a77f2a32
Elvonia <60554110+Elvonia@users.noreply.github.com>
2025-03-09 03:13:29 -0400

    Callback OCD

--- src/Steam/Callbacks.cs
+++ src/Steam/Callbacks.cs
@@ -4,6 +4,20 @@ namespace MultiplayerMod.Steam
 {
     public class Callbacks
     {
+        public static void OnFriendJoined(GameLobbyJoinRequested_t callback, MultiplayerMod instance)
+        {
+            instance.currentLobbyID = callback.m_steamIDLobby;
+            SteamMatchmaking.JoinLobby(instance.currentLobbyID);
+        }
+
+        public static void OnLobbyChatUpdated(LobbyChatUpdate_t callback, MultiplayerMod instance)
+        {
+            if (callback.m_ulSteamIDLobby == (ulong)instance.currentLobbyID)
+            {
+                instance.debugUI.UpdateLobbyUI(instance.currentLobbyID);
+            }
+        }
+
         public static void OnLobbyCreated(LobbyCreated_t callback, MultiplayerMod instance)
         {
             if (callback.m_eResult == EResult.k_EResultOK)
@@ -17,20 +31,6 @@ namespace MultiplayerMod.Steam
         {
             instance.currentLobbyID = new CSteamID(callback.m_ulSteamIDLobby);
             instance.debugUI.UpdateLobbyUI(instance.currentLobbyID);
-        }
-
-        public static void OnFriendJoined(GameLobbyJoinRequested_t callback, MultiplayerMod instance)
-        {
-            instance.currentLobbyID = callback.m_steamIDLobby;
-            SteamMatchmaking.JoinLobby(instance.currentLobbyID);
-        }
-
-        public static void OnLobbyChatUpdated(LobbyChatUpdate_t callback, MultiplayerMod instance)
-        {
-            if (callback.m_ulSteamIDLobby == (ulong)instance.currentLobbyID)
-            {
-                instance.debugUI.UpdateLobbyUI(instance.currentLobbyID);
-            }
         }
     }
 }
